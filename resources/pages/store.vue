59708120093cea3ad51dee0c7e931d35d7caeb74
Mario <alespcs@gmail.com>
2018-05-21 18:44:07 -0400

    add weblayout

--- resources/pages/store.vue
+++ resources/pages/store.vue
@@ -13,38 +13,13 @@
         <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt ea esse laudantium libero nesciunt quam quibusdam vel voluptatem? Accusantium delectus dolores fugiat incidunt perferendis quam quia quidem saepe voluptate voluptates?
         </p>
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt ea esse laudantium libero nesciunt quam quibusdam vel voluptatem? Accusantium delectus dolores fugiat incidunt perferendis quam quia quidem saepe voluptate voluptates?
-
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt ea esse laudantium libero nesciunt quam quibusdam vel voluptatem? Accusantium delectus dolores fugiat incidunt perferendis quam quia quidem saepe voluptate voluptates?
-
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt ea esse laudantium libero nesciunt quam quibusdam vel voluptatem? Accusantium delectus dolores fugiat incidunt perferendis quam quia quidem saepe voluptate voluptates?
-
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt ea esse laudantium libero nesciunt quam quibusdam vel voluptatem? Accusantium delectus dolores fugiat incidunt perferendis quam quia quidem saepe voluptate voluptates?
-
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt ea esse laudantium libero nesciunt quam quibusdam vel voluptatem? Accusantium delectus dolores fugiat incidunt perferendis quam quia quidem saepe voluptate voluptates?
-
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt ea esse laudantium libero nesciunt quam quibusdam vel voluptatem? Accusantium delectus dolores fugiat incidunt perferendis quam quia quidem saepe voluptate voluptates?
-
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt ea esse laudantium libero nesciunt quam quibusdam vel voluptatem? Accusantium delectus dolores fugiat incidunt perferendis quam quia quidem saepe voluptate voluptates?
-
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt ea esse laudantium libero nesciunt quam quibusdam vel voluptatem? Accusantium delectus dolores fugiat incidunt perferendis quam quia quidem saepe voluptate voluptates?
-
-
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt ea esse laudantium libero nesciunt quam quibusdam vel voluptatem? Accusantium delectus dolores fugiat incidunt perferendis quam quia quidem saepe voluptate voluptates?
-
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt ea esse laudantium libero nesciunt quam quibusdam vel voluptatem? Accusantium delectus dolores fugiat incidunt perferendis quam quia quidem saepe voluptate voluptates?
-
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt ea esse laudantium libero nesciunt quam quibusdam vel voluptatem? Accusantium delectus dolores fugiat incidunt perferendis quam quia quidem saepe voluptate voluptates?
-
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt ea esse laudantium libero nesciunt quam quibusdam vel voluptatem? Accusantium delectus dolores fugiat incidunt perferendis quam quia quidem saepe voluptate voluptates?
-        </p>
     </div>
 </template>
 
 <script>
     export default {
-        name: "about.vue"
+        name: "about.vue",
+        layout: "web-layout"
     }
 </script>
 
