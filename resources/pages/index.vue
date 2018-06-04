e4435f396f77127b471dd468d6b52ac623e3d6fd
Mario <alespcs@gmail.com>
2018-06-03 20:27:43 -0400

    changes env

--- resources/pages/index.vue
+++ resources/pages/index.vue
@@ -1,33 +1,7 @@
 <template>
     <section class="container">
         <h4><strong>Material design inputs</strong></h4>
-        <div style="margin-top:3rem;max-width:20rem">
-            <md-input type="text" label="Basic example"></md-input>
-            <md-input size="sm" type="text" label="Small input"></md-input>
-        </div>
-        <div style="margin-top:3rem;max-width:20rem">
-            <md-input type="text" label="Input with icon" icon="envelope" ></md-input>
-        </div>
-        <div style="margin-top:3rem;max-width:20rem">
-            <md-input type="text" label="Example label" placeholder="Placeholder" ></md-input>
-        </div>
-        <div style="margin-top:3rem;max-width:20rem">
-            <md-input type="text" label="Basic example" disabled ></md-input>
-        </div>
-        <div style="margin-top:3rem;max-width:20rem">
-            <md-input type="email" label="Your e-mail" ></md-input>
-        </div>
-        <div style="margin-top:3rem;max-width:20rem">
-            <md-input type="password" label="Password" ></md-input>
-        </div>
-        <div style="margin-top:3rem;max-width:20rem">
-            <md-textarea label="Textarea" ></md-textarea>
-        </div>
-        <h4 class="mt-5 mb-3"><strong>Default inputs</strong></h4>
-        <div style="max-width:20rem">
-            <label for="exampleForm2">Default input</label>
-            <input type="text" id="exampleForm2" class="form-control">
-        </div>
+
     </section>
 </template>
 
